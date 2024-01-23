183093e137743919713efdc5d0f97061f35c45a8
Situn09 <106417684+Situn09@users.noreply.github.com>
2024-01-23 22:50:05 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     domains: [
       "lh3.googleusercontent.com",
@@ -17,7 +18,6 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  assetPrefix: "./",
 };
 
 module.exports = nextConfig;
