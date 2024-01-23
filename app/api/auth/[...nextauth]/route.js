178786f8817762d7379d9f1720ac23f7f23c5444
Situn09 <106417684+Situn09@users.noreply.github.com>
2024-01-23 23:50:21 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,16 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
+  providers: [
+    GoogleProvider({
+      clientId:
+        "1030235262357-k1qu4nnncichkgubk5l8smaqguoh6jo8.apps.googleusercontent.com",
+      clientSecret: "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",
+    }),
   ],
   secret: "hello1234567890",
-  debug: true
-})
+  debug: true,
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
